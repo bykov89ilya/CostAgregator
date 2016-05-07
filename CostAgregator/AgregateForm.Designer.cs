ad818f24cfa925a6a08ac1db88fb5d8cbb4eebd6
6UBeHb <bykov89ilya@gmail.com>
2016-05-08 02:15:05 +0300

    gitignore added first commit

---
+++ CostAgregator/AgregateForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CostAgregator
+{
+    partial class AgregateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateReportButton = new System.Windows.Forms.Button();
+            this.OpenTinkoffFileButton = new System.Windows.Forms.Button();
+            this.OpenCashFileButton = new System.Windows.Forms.Button();
+            this.listViewCash = new System.Windows.Forms.ListView();
+            this.listViewTinkoff = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // CreateReportButton
+            // 
+            this.CreateReportButton.Location = new System.Drawing.Point(417, 224);
+            this.CreateReportButton.Name = "CreateReportButton";
+            this.CreateReportButton.Size = new System.Drawing.Size(101, 23);
+            this.CreateReportButton.TabIndex = 0;
+            this.CreateReportButton.Text = "Создать отчет";
+            this.CreateReportButton.UseVisualStyleBackColor = true;
+            this.CreateReportButton.Click += new System.EventHandler(this.CreateReportButton_Click);
+            // 
+            // OpenTinkoffFileButton
+            // 
+            this.OpenTinkoffFileButton.Location = new System.Drawing.Point(12, 12);
+            this.OpenTinkoffFileButton.Name = "OpenTinkoffFileButton";
+            this.OpenTinkoffFileButton.Size = new System.Drawing.Size(158, 23);
+            this.OpenTinkoffFileButton.TabIndex = 1;
+            this.OpenTinkoffFileButton.Text = "Открыть отчеты Тинькофф";
+            this.OpenTinkoffFileButton.UseVisualStyleBackColor = true;
+            this.OpenTinkoffFileButton.Click += new System.EventHandler(this.OpenTinkoffFileButton_Click);
+            // 
+            // OpenCashFileButton
+            // 
+            this.OpenCashFileButton.Location = new System.Drawing.Point(13, 144);
+            this.OpenCashFileButton.Name = "OpenCashFileButton";
+            this.OpenCashFileButton.Size = new System.Drawing.Size(157, 23);
+            this.OpenCashFileButton.TabIndex = 3;
+            this.OpenCashFileButton.Text = "Открыть отчет Наличка";
+            this.OpenCashFileButton.UseVisualStyleBackColor = true;
+            this.OpenCashFileButton.Click += new System.EventHandler(this.openCashFileButton_Click);
+            // 
+            // listViewCash
+            // 
+            this.listViewCash.Location = new System.Drawing.Point(12, 174);
+            this.listViewCash.Name = "listViewCash";
+            this.listViewCash.Size = new System.Drawing.Size(506, 44);
+            this.listViewCash.TabIndex = 4;
+            this.listViewCash.UseCompatibleStateImageBehavior = false;
+            this.listViewCash.View = System.Windows.Forms.View.List;
+            // 
+            // listViewTinkoff
+            // 
+            this.listViewTinkoff.Location = new System.Drawing.Point(13, 42);
+            this.listViewTinkoff.Name = "listViewTinkoff";
+            this.listViewTinkoff.Size = new System.Drawing.Size(505, 96);
+            this.listViewTinkoff.TabIndex = 5;
+            this.listViewTinkoff.UseCompatibleStateImageBehavior = false;
+            this.listViewTinkoff.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // AgregateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 259);
+            this.Controls.Add(this.listViewTinkoff);
+            this.Controls.Add(this.listViewCash);
+            this.Controls.Add(this.OpenCashFileButton);
+            this.Controls.Add(this.OpenTinkoffFileButton);
+            this.Controls.Add(this.CreateReportButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "AgregateForm";
+            this.ShowIcon = false;
+            this.Text = "Агрегатор отчетов";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateReportButton;
+        private System.Windows.Forms.Button OpenTinkoffFileButton;
+        private System.Windows.Forms.Button OpenCashFileButton;
+        private System.Windows.Forms.ListView listViewCash;
+        private System.Windows.Forms.ListView listViewTinkoff;
+    }
+}
+
---
+++ CostAgregator/AgregateForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CostAgregator
+{
+    public partial class AgregateForm : Form
+    {
+        public AgregateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenTinkoffFileButton_Click(object sender, EventArgs e)
+        {
+            listViewTinkoff.Items.Clear();
+
+            Stream streamTinkoff = null;
+            OpenFileDialog openFileDialogTinkoff = new OpenFileDialog();
+
+            openFileDialogTinkoff.InitialDirectory = "c:\\";
+            openFileDialogTinkoff.Filter = "csv files (*.csv)|*.csv";
+            openFileDialogTinkoff.FilterIndex = 1;
+            openFileDialogTinkoff.RestoreDirectory = true;
+            openFileDialogTinkoff.Multiselect = true;
+
+            if (openFileDialogTinkoff.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((streamTinkoff = openFileDialogTinkoff.OpenFile()) != null)
+                    {
+                        using (streamTinkoff)
+                        {
+                            foreach (var fileName in openFileDialogTinkoff.FileNames)
+                            {
+                                listViewTinkoff.Items.Add(new ListViewItem(fileName));
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не могу прочитать файл Тинькофф: " + ex.Message);
+                }
+            }
+        }
+
+        private void openCashFileButton_Click(object sender, EventArgs e)
+        {
+            listViewCash.Items.Clear();
+
+            Stream streamCash = null;
+            OpenFileDialog openFileDialogCash = new OpenFileDialog();
+
+            openFileDialogCash.InitialDirectory = "c:\\";
+            openFileDialogCash.Filter = "xls files (*.xls)|*.xls";
+            openFileDialogCash.FilterIndex = 1;
+            openFileDialogCash.RestoreDirectory = true;
+            openFileDialogCash.Multiselect = false;
+
+            if (openFileDialogCash.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((streamCash = openFileDialogCash.OpenFile()) != null)
+                    {
+                        using (streamCash)
+                        {
+                            foreach (var fileName in openFileDialogCash.FileNames)
+                            {
+                                listViewCash.Items.Add(new ListViewItem(fileName));
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не могу прочитать файл Наличка: " + ex.Message);
+                }
+            }
+        }
+
+        private void CreateReportButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listViewCash.Items.Count == 1 && listViewTinkoff.Items.Count >= 1
+                    && listViewTinkoff.Items.Count < 3)
+                {
+
+
+                    List<Report> list = new List<Report>();
+
+                    Excel.Application xlApp;
+                    Excel.Workbook xlWorkBook;
+                    Excel.Worksheet xlWorkSheet;
+                    Excel.Range range;
+
+                    int rCnt = 0;
+                    int cCnt = 0;
+
+                    xlApp = new Excel.Application();
+                    xlWorkBook = xlApp.Workbooks.Open(listViewCash.Items[0].Text, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                    xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                    range = xlWorkSheet.UsedRange;
+
+                    for (rCnt = 2; rCnt <= range.Rows.Count; rCnt++)
+                    {
+                        list.Add(new Report()
+                        {
+                            date = DateTime.FromOADate((double)(range.Cells[rCnt, 10] as Excel.Range).Value2),
+                            value = (double)(range.Cells[rCnt, 6] as Excel.Range).Value2,
+                            category = (string)(range.Cells[rCnt, 9] as Excel.Range).Value2,
+                            description = (string)(range.Cells[rCnt, 4] as Excel.Range).Value2
+                        });
+                    }
+
+                    xlWorkBook.Close(true, null, null);
+                    xlApp.Quit();
+
+                    releaseObject(xlWorkSheet);
+                    releaseObject(xlWorkBook);
+                    releaseObject(xlApp);
+
+                    for (int i = 0; i < listViewTinkoff.Items.Count; i++)
+                    {
+                        List<string> l = new List<string>();
+                        StreamReader sr = File.OpenText(listViewCash.Items[i].Text);
+                        while (!sr.EndOfStream)
+                        {
+                            l.Add(sr.ReadLine());
+                        }
+                        foreach (var repLine in File.ReadAllLines(listViewTinkoff.Items[i].Text)
+                            .Where(line => line.Split(new[] { ';' }).ElementAt(3).Replace("\"", "") == "OK"
+                                && Double.Parse(line.Split(new[] { ';' }).ElementAt(4).Replace("\"", "")) < 0)
+                            )
+                        {
+                            list.Add(new Report()
+                            {
+                                category = repLine.Split(new[] { ';' }).ElementAt(8).Replace("\"", ""),
+                                date = DateTime.Parse(repLine.Split(new[] { ';' }).ElementAt(0).Replace("\"", "")),
+                                description = repLine.Split(new[] { ';' }).ElementAt(10).Replace("\"", ""),
+                                value = Double.Parse(repLine.Split(new[] { ';' }).ElementAt(4).Replace("\"", "").Replace("-", ""))
+                            });
+                        }
+                    }
+
+                    Excel.Application xlAppReport;
+                    Excel.Workbook xlWorkBookReport;
+
+                    xlAppReport = new Excel.Application();
+                    xlWorkBookReport = xlAppReport.Workbooks.Add();
+                    Excel.Worksheet xlWorkSheetReport = xlWorkBookReport.Worksheets[1];
+
+                    var distCat = list.Select(item => item.category).Distinct().OrderBy(item => item);
+
+                    int row = 1;
+
+                    foreach (var cat in distCat)
+                    {
+                        var catList = list
+                            .Where(item => item.category == cat)
+                            .OrderBy(item => item.date)
+                            .ThenBy(item => item.value)
+                            .ThenBy(item => item.description);
+
+                        int headerRow = row;
+                        double headerSum = 0;
+                        xlWorkSheetReport.Cells[row, 1] = cat.ToString();
+                        row++;
+                        foreach (var el in catList)
+                        {
+                            headerSum += el.value;
+                            xlWorkSheetReport.Cells[row, 2] = el.date;
+                            xlWorkSheetReport.Cells[row, 3] = el.value;
+                            xlWorkSheetReport.Cells[row, 4] = el.description;
+                            row++;
+                        }
+                        xlWorkSheetReport.Cells[headerRow, 3] = headerSum;
+                    }
+
+                    string path = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToLongDateString().Replace(":", "_") + ".xlsx";
+                    if (File.Exists(path))
+                        File.Delete(path);
+                    xlWorkBookReport.SaveAs(Filename: path);
+                    MessageBox.Show("Отчет " + path + " успешно создан");
+
+                    releaseObject(xlWorkSheetReport);
+                    releaseObject(xlWorkBookReport);
+                    releaseObject(xlAppReport);
+                }
+                else
+                {
+                    MessageBox.Show("Количество отчетов Наличка не равно одному или количество отчетов Тинькофф не равно 1 или 2");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при создании отчета: " + ex.Message);
+            }
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+
+    struct Report
+    {
+        public DateTime date { get; set; }
+        public double value { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+    }
+}
